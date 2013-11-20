aa028bfcc5554ddd3657a29b2439a4a8f0fb4a9d
fconstantin <francois.constantin@kleegroup.com>
2013-11-20 13:26:50 +0100

    Création application Web

--- src/skymonitor/airspaceloader/LectureFichier.java
+++ src/skymonitor/airspaceloader/LectureFichier.java
@@ -4,18 +4,67 @@ import java.io.*;
 
 import com.mongodb.*;
 
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.LinkedList;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+
 public class LectureFichier {
-		
+
 	public static void main(String[] args) {
+		String fichier = "uk_air_2002.txt";
 		try {
-			Mongo mongo = new Mongo ("localhost", 27017);
-			DB db = mongo.getDB("db");
-			DBCollection zones = db.getCollection("zonesUk");
+			InputStream ips = new FileInputStream(fichier);
+			loadZones(ips, "localhost", "db");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static String processRequest(HttpServletRequest request) {
+		if ("POST".equals(request.getMethod())) {
 			
+			FileItemFactory factory = new DiskFileItemFactory();
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			upload.setSizeMax(10000000);// 10 Mo
+			
+			try {
+				List<FileItem> files = upload.parseRequest(request);
+				for (FileItem file : files) {
+					String server = request.getServletContext().getInitParameter("mongoserver");
+					String database = request.getServletContext().getInitParameter("mongodatabase");
+					loadZones(file.getInputStream(), server, database);
+					return "Zone charg&eacute;e";
+				}
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			} catch (FileUploadException e) {
+				e.printStackTrace();
+			}
+			
+			return request.getParameter("airspace");
+		}
+		
+		return "Chargement d'un fichier : (" + request.getServletContext().getInitParameter("mongodatabase") +")";
+	}
+
+	public static void loadZones(InputStream ips, String server, String database) {
+		try {
+			Mongo mongo = new Mongo(server, 27017);
+			DB db = mongo.getDB(database);
+			DBCollection zones = db.getCollection("zone");
+
 			ACCase AC = new ACCase();
 			AHCase AH = new AHCase();
 			ALCase AL = new ALCase();
@@ -26,55 +75,45 @@ public class LectureFichier {
 			DACase DA = new DACase();
 			DBCase DB = new DBCase();
 			DCCase DC = new DCCase();
-			
-			Case[] Possibilites = {AC,AH,AL,AN,Vdir,Vpoint,DP,DA,DB,DC};
-			
-			String fichier = "uk_air_2002.txt";
-			
-			try {
-				InputStream ips = new FileInputStream(fichier);
-				InputStreamReader ipsr=new InputStreamReader(ips);
-				BufferedReader br=new BufferedReader(ipsr);
-				String line;
-				BasicDBObject occ = new BasicDBObject();
-				occ.put("Starter", 0);
-				
-				int lineNbr = 0;
-				List errorReport = new LinkedList();
-				
-				while ((line=br.readLine()) != null) {
-					lineNbr++;
-					System.out.println(line);
-					String Line = line.toUpperCase();
-					for (Case poss : Possibilites) {
-						if (poss.matches(Line)) {
-							try {
-								poss.execute(Line, occ, zones);
-							} catch (Exception e) {
-								errorReport.add(lineNbr);
-							}
+
+			Case[] Possibilites = { AC, AH, AL, AN, Vdir, Vpoint, DP, DA, DB,
+					DC };
+
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String line;
+			BasicDBObject occ = new BasicDBObject();
+			occ.put("Starter", 0);
+
+			int lineNbr = 0;
+			List errorReport = new LinkedList();
+
+			while ((line = br.readLine()) != null) {
+				lineNbr++;
+				System.out.println(line);
+				String Line = line.toUpperCase();
+				for (Case poss : Possibilites) {
+					if (poss.matches(Line)) {
+						try {
+							poss.execute(Line, occ, zones);
+						} catch (Exception e) {
+							errorReport.add(lineNbr);
 						}
 					}
 				}
-				br.close();
-				
-				occ.removeField("Vpoint");
-				occ.removeField("Vdir");
-				zones.insert(occ);
-				
-				BasicDBObject Starter = new BasicDBObject();
-				Starter.put("Starter", 0);
-				zones.remove(Starter);
-				
-				System.out.println(errorReport);
 			}
-			catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-		catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (MongoException e) {
+			br.close();
+
+			occ.removeField("Vpoint");
+			occ.removeField("Vdir");
+			zones.insert(occ);
+
+			BasicDBObject Starter = new BasicDBObject();
+			Starter.put("Starter", 0);
+			zones.remove(Starter);
+
+			System.out.println(errorReport);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- web/index.jsp
+++ web/index.jsp
@@ -2,5 +2,9 @@
 <body>
 <h1>SkyMonitor</h1>
 <%= 1 + 1 %>
+
+<ul>
+<li><a href='admin/airspaceloader.jsp'>Chargement d'un zone</a>
+</ul>
 </body>
 </html>
