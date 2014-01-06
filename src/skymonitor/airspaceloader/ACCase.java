47484efd97bcd32703d82c023363fa0db9bad4c3
giulioforesto <giulioforesto@gmail.com>
2014-01-06 11:51:53 +0100

    fixed bug: Vdir is reset to + for each zone

--- src/skymonitor/airspaceloader/ACCase.java
+++ src/skymonitor/airspaceloader/ACCase.java
@@ -11,13 +11,11 @@ public class ACCase extends Case {
 	@Override
 	public void execute(String line, BasicDBObject occ, DBCollection coll) {
 		Object Vpoint = occ.get("Vpoint");
-		Object Vdir = occ.get("Vdir");
 		
 		LectureFichier.completeAndInsert(occ, coll);
 		
 		occ.clear();
 		occ.put("Vpoint", Vpoint);
-		occ.put("Vdir", Vdir);
 		
 		String chaine = line.substring(3).replaceAll("\\u002A.*", "");
 		occ.put("Type", chaine);
