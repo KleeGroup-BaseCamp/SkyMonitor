a3e236a8c87cb031ab9dd7c97407abba8b2f23e3
giulioforesto <giulioforesto@gmail.com>
2014-03-03 10:09:02 +0100

    added a try for homebutton + minor changes

--- cesium/body.js
+++ cesium/body.js
@@ -7,6 +7,16 @@ var scene = viewer.scene;
 var primitives = scene.getPrimitives();
 var centralBody = scene.getPrimitives().getCentralBody();
 centralBody.depthTestAgainstTerrain = true;
+/*
+var homeButtonCommand = Cesium.createCommand(function() {
+	var destination = Cesium.Cartographic.fromDegrees(2.351944, 48.856667, 15000.0);
+
+	var flight = Cesium.CameraFlightPath.createAnimationCartographic(scene, {
+		destination : destination
+	});
+	scene.getAnimations().add(flight);
+});
+*/
 
 var cesiumTerrainProvider = new Cesium.CesiumTerrainProvider({
 	url : 'http://cesiumjs.org/smallterrain',
@@ -26,7 +36,7 @@ var terrainProviders = [
 	{ name : 'CesiumTerrainProvider', provider : cesiumTerrainProvider }
 ];
 
-centralBody.terrainProvider = ellipsoidProvider;
+centralBody.terrainProvider = ellipsoidProvider; // Default terrainProvider
 
 createTerrainMenu(terrainProviders);
 
