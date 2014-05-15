a8d9ff097de650c2176b63c7098508b84039039d
giulioforesto <giulioforesto@users.noreply.github.com>
2014-05-15 17:45:54 +0200

    added proxy to datacollector and merge driver to keep it local

--- src/skymonitor/datacollector/ConnectJS.java
+++ src/skymonitor/datacollector/ConnectJS.java
@@ -6,8 +6,9 @@ public class ConnectJS {
 
 	public static void connectionJS() {
 		try {
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.20.0.9", 3128));
 			URL sourceDonnees = new URL("http://db.flightradar24.com/zones/full_all.js");
-			URLConnection connexion = sourceDonnees.openConnection();
+			URLConnection connexion = sourceDonnees.openConnection(proxy);
 			Principal.flux = connexion.getInputStream();
 		} catch (Exception e) {
 			e.printStackTrace();
