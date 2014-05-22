91161382ba41e5509e81ec8473f59c7b4505af23
giulioforesto <giulioforesto@users.noreply.github.com>
2014-05-22 15:17:52 +0200

    Create ConnectJS.java

---
+++ src/skymonitor/datacollector/ConnectJS.java
@@ -0,0 +1,16 @@
+package skymonitor.datacollector;
+
+import java.net.*;
+
+public class ConnectJS {
+
+	public static void connectionJS() {
+		try {
+			URL sourceDonnees = new URL("http://db.flightradar24.com/zones/full_all.js");
+			URLConnection connexion = sourceDonnees.openConnection();
+			Principal.flux = connexion.getInputStream();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
